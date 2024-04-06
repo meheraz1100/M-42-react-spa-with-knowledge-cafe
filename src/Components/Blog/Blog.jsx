156ac0fe2af3273df1a8d97061732dd33adad554
meheraz1100 <meheraz1100@gmail.com>
2024-04-06 09:40:36 +0600

    show data in UI

--- src/Components/Blog/Blog.jsx
+++ src/Components/Blog/Blog.jsx
@@ -1,16 +1,53 @@
-import PropTypes from 'prop-types';
+import PropTypes from "prop-types";
+import { BsBookmarks } from "react-icons/bs";
 
 const Blog = ({ blog }) => {
-    console.log(blog);
-    return (
-        <div>
-            
+  const {
+    cover,
+    title,
+    author_img,
+    author,
+    posted_date,
+    reading_time,
+    hashtags,
+  } = blog;
+  return (
+    <div className="mb-20">
+      <img
+        className="w-full mb-8"
+        src={cover}
+        alt={`cover picture of the title ${title}`}
+        title={title}
+      />
+      <div className="flex justify-between mb-4">
+        <div className="flex">
+          <img className="w-14" src={author_img} alt="" />
+          <div className="ml-6">
+            <h3 className="text-2xl">{author}</h3>
+            <p>{posted_date}</p>
+          </div>
         </div>
-    );
+        <div>
+          <span>{reading_time} min read</span>
+          <button className="ml-2 text-2xl text-red-600">
+            <BsBookmarks />
+          </button>
+        </div>
+      </div>
+      <h2 className="text-4xl">Title: {title}</h2>
+      <p>
+        {hashtags.map((hash, idx) => (
+          <span key={idx}>
+            <a href="">#{hash}</a>{" "}
+          </span>
+        ))}
+      </p>
+    </div>
+  );
 };
 
 Blog.propTypes = {
-    blog: PropTypes.object.isRequired
-}
+  blog: PropTypes.object.isRequired,
+};
 
 export default Blog;
