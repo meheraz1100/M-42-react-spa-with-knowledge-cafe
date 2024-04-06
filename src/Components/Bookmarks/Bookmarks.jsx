9409ab34261cded9e99b60f55fd20c4369505c53
meheraz1100 <meheraz1100@gmail.com>
2024-04-07 00:13:59 +0600

    added reading time functionality

--- src/Components/Bookmarks/Bookmarks.jsx
+++ src/Components/Bookmarks/Bookmarks.jsx
@@ -1,9 +1,23 @@
-const Bookmarks = () => {
+import PropTypes from 'prop-types';
+import Bookmark from '../Bookmark/Bookmark';
+
+const Bookmarks = ({ bookmarks, readingTime }) => {
     return (
-        <div className="md:w-1/3">
-            <h2 className="text-xl">Bookmarks</h2>       
+        <div className="md:w-1/3 bg-gray-300 ml-4 mt-2 rounded-xl pt-4">
+            <div className='m-4 p-3 text-center bg-yellow-400 rounded-xl'>
+                <h3 className='text-2xl'>Reading Time: {readingTime}</h3>
+            </div>
+            <h2 className="text-xl text-center ">Bookmarked Blogs : {bookmarks.length}</h2>
+            {
+                bookmarks.map((bookmark, idx) => <Bookmark key={idx} bookmark={bookmark}></Bookmark>)
+            }      
         </div>
     );
+};
+
+Bookmarks.propTypes = {
+    bookmarks: PropTypes.array,
+    readingTime: PropTypes.number
 };
 
 export default Bookmarks;
